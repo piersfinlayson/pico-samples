3d3ebdb830764b9b9d0c9a04cf41237ce67610e0
Piers Finlayson <piers@piersandkatie.com>
2023-09-08 06:50:06 +0100

    require uf2 to be added

--- uarts/flash-pico.sh
+++ uarts/flash-pico.sh
@@ -3,7 +3,7 @@ set -e
 DEV=$1
 IMAGE=$2
 if [ -z $DEV ] || [ $DEV = "" ] || [ -z $IMAGE ] || [ IMAGE = "" ]; then
-  printf "Usage: %s <device> <image (without .uf2 extension)>\n" $0
+  printf "Usage: %s <device> <image (with .uf2 extension)>\n" $0
   printf "  e.g. %s sda mbus\n" $0
   exit 1
 fi
@@ -15,9 +15,9 @@ printf "Ensure not yet mounted\n"
 sudo umount $MOUNT_DIR
 sudo umount $DEVICE
 set -e
-printf "Copying %s to %s\n" ${IMAGE}.uf2 $DEVICE
+printf "Copying %s to %s\n" ${IMAGE} $DEVICE
 sudo mount $DEVICE $MOUNT_DIR
-sudo cp ${IMAGE}.uf2 $MOUNT_DIR/
+sudo cp ${IMAGE} $MOUNT_DIR/
 sudo umount $MOUNT_DIR
 printf "Copied\n"
 sync
